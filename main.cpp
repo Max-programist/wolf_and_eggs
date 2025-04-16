2d5caac78c7265ae2a25e55fac323921a555e159
Max <makskiller1511@gmail.com>
2025-04-16 16:46:38 +0300

    final version game

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <unistd.h>
 #include <ncurses.h>
 #include <vector>
-#include <sys/select.h>
 #include <fcntl.h>
 #include <thread>
 #include <chrono>
@@ -14,9 +13,10 @@ struct wolf {
   int i;
   int j;
   uint speed;
+  bool pause;
 };
 
-struct wolf position_wolf = {A - 1, B / 2, 1000000000};
+struct wolf wolf_stat = {A - 1, B / 2, 1000000000, false};
 std::vector<std::vector<char>> create();
 void  fill(std::vector<std::vector<char>>  &arr);
 void print(const std::vector<std::vector<char>>& arr, const int* points);
@@ -31,6 +31,8 @@ int main() {
   
   srand(time(0));
   
+
+
   int flags = fcntl(0, F_GETFL, 0);
   fcntl(0, F_SETFL, flags | O_NONBLOCK);
 
@@ -41,19 +43,21 @@ int main() {
   int point = 0;
   
   
-  
+
   for( ; ; ) {
     wolf_controller(arr);
-    speed(point);
-    bool check = check_eggs(arr, &point);
-    move_eggs(arr);
-    generation_eggs(arr);
-    print(arr, &point);
-    if(check == true) {
-      break;
+    if(wolf_stat.pause == false) {
+      bool check = check_eggs(arr, &point);
+      move_eggs(arr);
+      generation_eggs(arr);
+      print(arr, &point);
+      if(check == true) {
+        break;
+      }
+      std::this_thread::sleep_for(std::chrono::nanoseconds(wolf_stat.speed));
+      
     }
-    
-    std::this_thread::sleep_for(std::chrono::nanoseconds(position_wolf.speed));
+   
     
   }
   std::cout << "Game over" << std::endl;
@@ -86,7 +90,7 @@ std::vector<std::vector<char>> create() {
       for(int j = 0; j < B; j++) {
         
         arr[i][j] = '.';
-        arr[position_wolf.i][position_wolf.j] = 'w';
+        arr[wolf_stat.i][wolf_stat.j] = 'w';
         if(i == 0 || j == 0 || j == B - 1) {
           arr[i][j] = '|';
         }
@@ -113,7 +117,7 @@ void print(const std::vector<std::vector<char>>& arr, const int* points) {
 
 void generation_eggs(std::vector<std::vector<char>>& arr) {
 
-  if(rand() % 100 >= 50) {
+  if(rand() % 100 >= 75) {
 
     int egg = rand() % (B - 2) + 1;
     arr[1][egg] = 'o'; 
@@ -140,8 +144,9 @@ bool check_eggs(std::vector<std::vector<char>>& arr, int* points) {
   for(int i = 1; i < B - 1; i++) {
     if(arr[A - 2][i] == 'o') {
 
-      if(position_wolf.j == i) {
+      if(wolf_stat.j == i) {
         (*points)++;
+        speed(*points);
         arr[A - 2][i] = '.';
       } else {
         arr[A - 2][i] = '.'; 
@@ -161,40 +166,73 @@ void wolf_controller(std::vector<std::vector<char>>& arr) {
     if(c == 'q') {
       system("stty sane");
       exit(0);
+    
 
-    } else if(c == 'd') {
-      arr[position_wolf.i][position_wolf.j] = '.';
-      position_wolf.j = 3;
-      arr[position_wolf.i][position_wolf.j] = 'w';
+    } else if((c == 'd')  && (wolf_stat.pause != true)) {
+      arr[wolf_stat.i][wolf_stat.j] = '.';
+      wolf_stat.j = 3;
+      arr[wolf_stat.i][wolf_stat.j] = 'w';
 
-    } else if(c == 'a') {
-      arr[position_wolf.i][position_wolf.j] = '.';
-      position_wolf.j = 1;
-      arr[position_wolf.i][position_wolf.j] = 'w';
+    } else if((c == 'a')  && (wolf_stat.pause != true)) {
+      arr[wolf_stat.i][wolf_stat.j] = '.';
+      wolf_stat.j = 1;
+      arr[wolf_stat.i][wolf_stat.j] = 'w';
  
-    } else if(c == 's') {
-      arr[position_wolf.i][position_wolf.j] = '.';
-      position_wolf.j = 2;
-      arr[position_wolf.i][position_wolf.j] = 'w';
-    } else if(c == 'f') {
-      arr[position_wolf.i][position_wolf.j] = '.';
-      position_wolf.j = 4;
-      arr[position_wolf.i][position_wolf.j] = 'w';
+    } else if((c == 's')  && (wolf_stat.pause != true)) {
+      arr[wolf_stat.i][wolf_stat.j] = '.';
+      wolf_stat.j = 2;
+      arr[wolf_stat.i][wolf_stat.j] = 'w';
+    } else if((c == 'f') && (wolf_stat.pause != true)) {
+      arr[wolf_stat.i][wolf_stat.j] = '.';
+      wolf_stat.j = 4;
+      arr[wolf_stat.i][wolf_stat.j] = 'w';                 
+    } else if(c == 'p') {
+      wolf_stat.pause = !wolf_stat.pause;
+    } else if(c == 'm') {
+      wolf_stat.speed /= 1.1;
+    }else if(c == 'n') {
+      wolf_stat.speed *= 1.1;
     }
 
-  
+
+    
   }  
 }
 
+
+
+
+
+
+
 void speed(const uint& points) {
 
-                                                //4 200 000 000 * 1.1 = 4 620 000 000 
-  if((points % 5 == 0) && (position_wolf.speed <  4294967295 - position_wolf.speed * 0.1)) {
-    position_wolf.speed *= 1.1;
+                                               //4 200 000 000 * 1.1 = 4 620 000 000 
+  if(points % 5 == 0) {
+    wolf_stat.speed /= 1.1;//1.1;
     //−2 147 483 648-----------------------------------0--------------------------------------------->2 147 483 648
     //0-------------------------------------------------------------------------------->  4 294 967 295
   }
 
 
 
+
+
+
+
+// 01111111
+//00000000
+//00000001 2^0
+//00000010 2^1
+//01111111
+//10000000 2^7 = 128
+//00000000 
+//00000010
+///10000000000000000000000100000001
+//                   +
+//                        1                          1
+//                       1000000000
 }
+
+
+
